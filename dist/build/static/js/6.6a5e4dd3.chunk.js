1f143f18c8e809067ed8a3c182e3eafa539503d2
Caio F. Curitiba Marcellos <caiocuritiba@gmail.com>
2019-04-08 13:30:17 -0300

    remove dist

--- dist/build/static/js/6.6a5e4dd3.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{5626:function(e,r,n){"use strict";n.r(r);var o=n(0),a=n.n(o),t=n(33),i=n(107),c=n(26);r.default=function(){return Object(c.c)("Avaliar Trabalho| PSE-2019"),a.a.createElement("div",null,a.a.createElement(t.d,{handleError:function(e){console.error(e),console.error("Oi erro"),Object(i.b)({message:"Erro na identifica\xe7\xe3o do dispositivo.",variant:"error"})}}),a.a.createElement(i.a,null))}}}]);
-//# sourceMappingURL=6.6a5e4dd3.chunk.js.map
